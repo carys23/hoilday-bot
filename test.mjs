48b3782e3ea6dc0691b3232fa1643503fc4219e5
carys <caryspritch@gmail.com>
2022-07-12 17:21:27 +0100

    added basic css

--- test.mjs
+++ test.mjs
@@ -1,7 +1,13 @@
 import {myjson} from './csv.mjs' 
 
-
+function myfunc(){
 console.log(myjson[1].HotelName)
+// document.getElementById("demo").innerHTML = myjson[1].HotelName;
+
+
+}
+
+export {myfunc};
 
 
 
