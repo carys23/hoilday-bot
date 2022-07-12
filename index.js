9bc501920ffc6ac72c030208e6e6cf8c81b76708
carys <caryspritch@gmail.com>
2022-07-12 10:48:16 +0100

    added
    json file
    uml

---
+++ index.js
@@ -0,0 +1,14 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./bot.db', sqlite3.OPEN_READWRITE, (err)=>{
+    if (err) return console.error(err.message);
+
+    console.log('connected to database')
+});
+
+// db.run("CREATE TABLE hotel (name, rating, price) ");
+db.run = "INSERT INTO hotel (name, rating, price) VALUES ('TooHot', 5 , 67)";
+
+db.close((err) => {
+    if (err) return console.error(err.message);
+});
